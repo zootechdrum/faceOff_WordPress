4cf3d89734c88a6b6aac538242a6e457bcd0378f
Cesar Gomez <zootechdrum@gmail.com>
2019-05-29 22:57:14 -0700

    added color change to text area

--- front-page.php
+++ front-page.php
@@ -2,13 +2,13 @@
 <?php get_header(); ?>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-4 col-sm nopadding">
+        <div class="col-lg-4 col-sm-6 col-sm nopadding">
             <img src="<?php echo get_template_directory_uri()?>/cam.jpg" <alt="business logo" />
         </div>
-        <div class="col-lg-4  col-sm nopadding">
+        <div class="col-lg-4 col-sm-6  col-sm nopadding">
             <img src="<?php echo get_template_directory_uri()?>/cam.jpg" <alt="business logo" />
         </div>
-        <div class="col-lg-4 col-sm nopadding">
+        <div class="col-lg-4 col-sm-6  col-sm nopadding">
             <img src="<?php echo get_template_directory_uri()?>/cam.jpg" <alt="business logo" />
         </div>
     </div>
