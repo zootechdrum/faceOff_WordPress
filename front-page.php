98e9516ef7cf7f207ef7177f12fb000ccd4da8f5
Cesar Gomez <zootechdrum@gmail.com>
2019-06-01 09:24:39 -0700

    added images added style to sidebar of blog section

--- front-page.php
+++ front-page.php
@@ -3,13 +3,13 @@
 <div class="container-fluid">
     <div class="row hide-overflow">
             <div class="col-lg-4 col-sm-6 col-sm nopadding">
-                <img src="<?php echo get_template_directory_uri()?>/cam.jpg" <alt="business logo" />
+                <img src="<?php echo get_template_directory_uri()?>/camcam.jpg" <alt="business logo" />
             </div>
             <div class="col-lg-4 col-sm-6  col-sm nopadding">
-                <img src="<?php echo get_template_directory_uri()?>/cam.jpg" <alt="business logo" />
+                <img src="<?php echo get_template_directory_uri()?>/camcam.jpg" <alt="business logo" />
             </div>
             <div class="col-lg-4 col-sm-6  col-sm nopadding">
-                <img src="<?php echo get_template_directory_uri()?>/cam.jpg" <alt="business logo" />
+                <img src="<?php echo get_template_directory_uri()?>/camcam.jpg" <alt="business logo" />
             </div>
     </div>
 </div>
